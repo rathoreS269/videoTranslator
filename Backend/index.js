0f9d6110eaf06e6c1a1474ad9f73ef4e8a202648
shruti rathore <rathoreshruti269@gmail.com>
2024-11-11 16:33:46 +0530

    created route

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import userRouter from './routes/video.route.js'
+dotenv.config();
 const app = express();
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
 }
 );
+
+app.use("/api/video", userRouter);
